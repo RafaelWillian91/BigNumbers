392146213056418845b68b3845b5685dede16dd1
rafaw <rafa.willian91@gmail.com>
2023-03-13 05:59:31 -0300

    Trabalhando com BigDecimal

---
+++ src/Bigdecimall.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+
+public class Bigdecimall {
+
+    public static void main(String[] args) {
+
+        BigDecimal big = new BigDecimal("1465444873.44");//Numeros double grandes
+
+        System.out.println(big);
+
+        //Assim como em BigInteger as operacoes com Bigdecimal são chamadas em métodos
+
+        big = big.multiply(BigDecimal.TEN);//Tambem imutável
+
+        System.out.println(big);
+
+        //Na conversão temos que cuidar com a extracao, para caber dentro do tipo primitivo
+        int a = big.intValue();
+
+        System.out.println(a);
+    }
+
+}
